1e76c7dd2dcd731adb9070b182029c8a3b58c9e6
Meb50 <meb50@kent.ac.uk>
2026-01-20 13:53:08 +0000

    All tasks complete

---
+++ if-elif-else.py
@@ -0,0 +1,17 @@
+score = int(input("Please enter your score: "))
+print(" Your grade is: ")
+
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80 and score <= 89:
+    print("B")
+elif score >= 70 and score <= 79:
+    print("C")
+elif score >= 60 and score <= 69:
+    print("D")
+elif score > 100:
+    print("That is not a possible score")
+elif score < 0:
+    print("That is not a possible score")
+else:
+    print("F")
